3f6b165af776d2e3cab0756fa5fb1bd83eced2ec
yingguang123 <1733630402@qq.com>
2020-12-30 14:57:41 +0800

    翟

--- src/views/H-Ade/components/MainPage.vue
+++ src/views/H-Ade/components/MainPage.vue
@@ -40,29 +40,29 @@
     </div>
     <!-- 内容 -->
     <el-table class="elTable" :data="tableData">
-      <el-table-column type="index" label="序号" width="80">
+      <el-table-column type="index" label="序号" width="110">
       </el-table-column>
-      <el-table-column prop="serialNum" label="事件编码" width="120">
+      <el-table-column prop="serialNum" label="事件编码" width="130">
       </el-table-column>
-      <el-table-column prop="name" label="患者姓名" width="120">
+      <el-table-column prop="name" label="患者姓名" width="130">
       </el-table-column>
-      <el-table-column prop="sex" label="性别" width="120">
+      <el-table-column prop="sex" label="性别" width="130">
       </el-table-column>
-      <el-table-column prop="age" label="年龄" width="120">
+      <el-table-column prop="age" label="年龄" width="130">
       </el-table-column>
       <el-table-column prop="date" label="事发日期" width="150">
       </el-table-column>
-      <el-table-column prop="place" label="发生场所" width="120">
+      <el-table-column prop="place" label="发生场所" width="130">
       </el-table-column>
-      <el-table-column prop="level" label="轻重程度" width="80">
+      <el-table-column prop="level" label="轻重程度" width="110">
       </el-table-column>
       <el-table-column prop="date1" label="上报时间" width="150">
       </el-table-column>
-      <el-table-column prop="depart" label="患者科室" width="120">
+      <el-table-column prop="depart" label="患者科室" width="140">
       </el-table-column>
-      <el-table-column prop="person" label="上报人" width="120">
+      <el-table-column prop="person" label="上报人" width="140">
       </el-table-column>
-      <el-table-column label="操作" width="120">
+      <el-table-column label="操作" width="140">
         <template slot-scope="scope">
           <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
         </template>
