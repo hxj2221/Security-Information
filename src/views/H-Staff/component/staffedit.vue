0572712e03ac138004373481d250d2b1125b707d
hxj2221 <3124992812@qq.com>
2021-01-28 18:01:33 +0800

    2021-1-28-13

--- src/views/H-Staff/component/staffedit.vue
+++ src/views/H-Staff/component/staffedit.vue
@@ -151,10 +151,7 @@
               <el-select
                 type="input"
                 autosize
-<<<<<<< HEAD
-=======
                 multiple
->>>>>>> 5cbdadc21e9ec1697080c41839beb7f0285eccf2
                 v-model="addStaff.auth_grouap"
                 placeholder="请选择"
                 @change="departsel"
@@ -277,7 +274,6 @@ export default {
       id: "",
     };
   },
-<<<<<<< HEAD
   watch: {
     childed(res) {
       console.log(res.role_id);
@@ -300,7 +296,7 @@ export default {
       this.addStaff.status = res.status;
       // this.addStaff.head_department = res.head_department;
       this.addStaff.position = res.position;
-      this.addStaff.auth_grouap = res.auth_grouap[0].id;
+      this.addStaff.auth_grouap = res.auth_grouap.id;
       this.role_id = res.role_id;
       this.addStaff.status = res.status;
     },
@@ -331,12 +327,6 @@ export default {
       };
       console.log(params);
       service.staffEdit(params).then((res) => {
-=======
-
-    watch: {
-      childed(res) {
-        console.log(res.role_id);
->>>>>>> 5cbdadc21e9ec1697080c41839beb7f0285eccf2
         console.log(res);
         if (res.code == 20010) {
           this.$message({
