804c0d751867f60ac0102a4094ee22be02764a52
yingguang123 <1733630402@qq.com>
2021-01-28 10:45:55 +0800

    翟-文章管理页面样式

--- src/views/H-Articlelist/component/articleList/index.vue
+++ src/views/H-Articlelist/component/articleList/index.vue
@@ -27,10 +27,10 @@
       </div>
       <!-- 表格部分 -->
       <div class="articleList_table_table">
-        <div style="min-height:720px">
+        <div style="min-height:590px">
            <!-- 表格 -->
         <el-table
-        max-height="662"
+        max-height="595"
           ref="multipleTable"
           :data="tableData"
           tooltip-effect="dark"
